f4b8eb3c31ef31c532ee1359c943965752202e77
Karan <karansethia24@gmail.com>
2023-12-01 18:58:16 +0530

    Update: adding access tokens

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,31 +4,44 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const postRegister = asyncWrapper(async(req,res)=>{
-    const {username, password} = req.body;
-    if(!username || !password){
-    return res.sendStatus(400).json({"message": "Username or password not found"}) //400 => bad req
+
+const postRegister = async(req,res)=>{
+  const {username, password} = req.body;
+  console.log(req.body);
+  if(!username || !password){
+    return res.status(400).json({"message": "Username or password not found"}) //400 => bad req
   }
   //find if user exists
-  try {
-    const hashedPwd = bcrypt.hash(password, 10);
-    const newUser = { "username": username, "password": hashedPwd };
-    await User.create(newUser);
-    res.status(201).json({"message": "created"})
-  } catch (error) {
-    res.status(500).json({"message": error.message})
-  }
-    const newUser = 
-    res.status(200).json({message: "User created",data: newUser}) 
-});
+   try {
+        const duplicate = await User.find({ username: username });
+
+        if (duplicate.length !== 0) {
+            return res.status(409).json({ "message": "Username already exists" });
+        }
+
+        const hashedPwd = await bcrypt.hash(password, 10); // Wait for the hashing to complete
+        const newUser = { "username": username, "password": hashedPwd };
+
+        await User.create(newUser);
+
+        // Send a 201 response with JSON
+        res.status(201).json({ "message": "User created successfully" });
+    } catch (error) {
+        // Send a 500 response with JSON
+        res.status(500).json({ "message": error.message });
+    }
+    
+};
 
 const postLogin = async(req,res) => {
   const { username, password } = req.body;
+  console.log(req.body);
   if(!username || !password){
     return res.status(400).json({"message": "Username or password not found"}) //400 => bad req
   }
   // find existing user
-  const foundUser = true;
+  const foundUser = await User.findOne({username: username});
+  console.log(foundUser);
   if(!foundUser){
     return res.status(401).json({"message": "User not found"}) //401 => unauthorized
   }
@@ -49,9 +62,9 @@ const postLogin = async(req,res) => {
     //saving refresh token in the database to the current user
     // sending refresh and access tokens
     res.cookie('jwt',refreshToken,{httpOnly: true})
-    res.sendStatus(200).json({accessToken});
+    res.status(200).json({accessToken});
     }else{
-        res.sendStatus(401).json({"message": "Incorrect username or password"})
+        res.status(401).json({"message": "Incorrect username or password"})
     }
 }
 
