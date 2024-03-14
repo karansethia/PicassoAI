a5a9d004ad1a0769dd72c745fd9106a90c2772d4
Karan <karansethia24@gmail.com>
2024-03-15 00:54:29 +0530

    Update: Adding health check

--- server/app.js
+++ server/app.js
@@ -30,6 +30,10 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
+app.get('/health',(req,res) => {
+res.json({message: "Health ok"})
+})
+
 //routes
 app.use('/api/v1',authRoutes);
 app.use('/api/v1',userRoutes);
